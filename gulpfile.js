6647c0ed1d7927bd2718954cfed36f881da169f9
gnadon <gnadon@bisscomm.com>
2021-05-08 13:21:21 -0400

    allow inject variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,46 @@
-var del = require('del');
-var gulp = require('gulp');
-var emailBuilder = require('./lib/emailBuilder');
-var connect = require('gulp-connect');
+const del = require('del'),
+    gulp = require('gulp'),
+    emailBuilder = require('./lib/emailBuilder'),
+    connect = require('gulp-connect'),
+    replaceInclude  = require('gulp-replace-include'),
+    filesToWatch = [
+        'src/html/*.html',
+        'src/css/*.css',
+        'src/data/*.json'
+    ],
+    config = require('./src/data/global.json');
 
-
-/* *************
-	CLEAN
-************* */
 
 gulp.task('clean', function (cb) {
-  return del([
-    './src/dist',
-    './reports'
-  ], cb);
+    return del([
+        './src/dist',
+        './reports'
+    ], cb);
 });
 
 
-/* *************
-	SERVER
-************* */
+gulp.task('connect', function () {
+    connect.server({
+        port: 8000,
+        root: './src',
+        livereload: true
+    });
+});
 
-gulp.task('connect', function() {
-  connect.server({
-    port: 8000,
-    root: './src', // Serve from build directory instead,
-    livereload:true
-  });
+gulp.task('build', function () {
+    return gulp.src(['./src/html/*.html'])
+
+        .pipe(emailBuilder().build())
+        .pipe(replaceInclude({
+            global: config,
+        }))
+        .pipe(gulp.dest('./src/dist/'))
+        .pipe(connect.reload());
 });
 
 
-/* *************
-	BUILD
-************* */
-
-gulp.task('build', function() {
-  return gulp.src(['./src/html/*.html'])
-    .pipe(emailBuilder().build())
-    .pipe(gulp.dest('./src/dist/'))
-    .pipe(connect.reload());
+gulp.task('watch', function () {
+    gulp.watch(filesToWatch, ['build']);
 });
-
-
-/* *************
-    WATCH
-************* */
-
-var filesToWatch = [
-  'src/html/*.html',
-  'src/css/*.css'
-]
-
-gulp.task('watch', function() {
-  gulp.watch(filesToWatch,['build']);
-});
-
-
-
-/* *************
-    DEFAULT
-************* */
 
 gulp.task('default', ['connect', 'build', 'watch']);

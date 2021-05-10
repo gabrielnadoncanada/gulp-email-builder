a2a36b12389563a8374762ddea3c87ddccc4f51c
gnadon <gnadon@bisscomm.com>
2021-05-09 23:15:05 -0400

    generate multiple files from one html entry

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
-const del = require('del'),
-    gulp = require('gulp'),
-    emailBuilder = require('./lib/emailBuilder'),
-    connect = require('gulp-connect'),
-    replaceInclude  = require('gulp-replace-include'),
-    filesToWatch = [
-        'src/html/*.html',
-        'src/css/*.css',
-        'src/data/*.json'
-    ],
-    config = require('./src/data/global.json');
+var gulp = require('gulp');
+var emailBuilder = require('./lib/emailBuilder');
+var connect = require('gulp-connect');
+var replaceInclude = require('gulp-replace-include');
+var filesToWatch = [
+    'src/html/*.html',
+    'src/css/*.css',
+    'src/data/global.json'
+];
+var configs = require('./src/data/global');
+var File = require('vinyl');
+var through2 = require('through2');
+var path = require('path');
 
 
-gulp.task('clean', function (cb) {
-    return del([
-        './src/dist',
-        './reports'
-    ], cb);
-});
-
+function generate_files() {
+    'use strict';
+    return through2.obj(function (file, enc, next) {
+        var mydata = file.contents.toString('utf8');
+        var base = path.join(file.path, '..');
+        var self = this;
+        configs.main.forEach(function(config, index){
+            self.push(new File({
+                base: base,
+                path: path.join(base, index + '.html'),
+                contents: new Buffer(mydata)
+            }));
+        });
+        next();
+    });
+}
 
 gulp.task('connect', function () {
     connect.server({
@@ -29,11 +40,8 @@ gulp.task('connect', function () {
 
 gulp.task('build', function () {
     return gulp.src(['./src/html/*.html'])
-
         .pipe(emailBuilder().build())
-        .pipe(replaceInclude({
-            global: config,
-        }))
+        .pipe(generate_files())
         .pipe(gulp.dest('./src/dist/'))
         .pipe(connect.reload());
 });

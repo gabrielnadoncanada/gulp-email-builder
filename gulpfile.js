08f2f266178a80ee098c273d0b7e4664ee1a69ae
gnadon <gnadon@bisscomm.com>
2021-05-14 15:34:50 -0400

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,46 @@ function generate_files() {
         var mydata = file.contents.toString('utf8');
         var base = path.join(file.path, '..');
         var self = this;
-        configs.main.forEach(function(config, index){
+        var html_start = "<html><head><meta charset='UTF-8'></head><body><h1>Index des signatures</h1><ul>";
+        var html_end = "</ul></body></head></html>";
+        var html_content = "";
+
+        configs.main.forEach(function (config, index) {
+            html_content += "<li><a href='./" + index + ".html'>" + config.name + "</a></li>";
+            let new_data = replaceTokens(config, mydata);
             self.push(new File({
                 base: base,
                 path: path.join(base, index + '.html'),
-                contents: new Buffer(mydata)
+                contents: new Buffer(new_data)
             }));
         });
+        this.push(new File({
+            base: base,
+            path: path.join(base, "index.html"),
+            contents: new Buffer(html_start + html_content + html_end)
+        }))
         next();
     });
 }
 
+function get(obj, desc) {
+    var arr = desc.split(".");
+    while (arr.length && (obj = obj[arr.shift()])) ;
+    return obj;
+}
+
+function replaceTokens(config, HTML) {
+    return HTML.split('{{').map(function (i) {
+        var symbol = i.substring(0, i.indexOf('}}')).trim();
+        return i.replace(symbol + '}}', get(config, symbol));
+    }).join('');
+}
+
+
 gulp.task('connect', function () {
     connect.server({
         port: 8000,
-        root: './src',
+        root: './',
         livereload: true
     });
 });
